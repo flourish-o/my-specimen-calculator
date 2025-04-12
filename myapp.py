1dcabd32b47f76a3d4abe9bbdfb334829f21ee95
flourish-o <163390292+flourish-o@users.noreply.github.com>
2025-04-12 18:16:52 +0100

    commit

--- myapp.py
+++ myapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -47,4 +48,7 @@ def index():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
+
